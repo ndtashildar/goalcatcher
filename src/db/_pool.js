8d4f58c52f1872a68dbf2722b9830c18e40a7986
shankar kailas <skailas1@asu.edu>
2022-11-24 01:26:29 -0700

    using default pg user

--- src/db/_pool.js
+++ src/db/_pool.js
@@ -1,11 +1,17 @@
 const Pool = require("pg").Pool;
 
+// using postgres user and pw as default
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  user: "postgres",
+  host: "localhost",
   database: "goalcatcher",            // For some reason this isn't taking an env variable
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  password: "postgres",
+  port: 5432,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: "goalcatcher",            // For some reason this isn't taking an env variable
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- src/middleware/_test.js
+++ src/middleware/_test.js
@@ -3,7 +3,7 @@ const {pool} = require("../db");
 module.exports = async () => {
   try {
     const queryResult = await pool.query(
-      `SELECT matches.home_score, matches.away_score, plays.away_id FROM matches INNER JOIN plays ON matches.mid = plays.mid INNER JOIN teams ON teams.tid = plays.home_id WHERE teams.team_name = 'England'`
+      `SELECT matches.home_score, matches.away_score, plays.away_id FROM matches INNER JOIN plays ON matches.mid = plays.mid INNER JOIN teams ON teams.tid = plays.home_id WHERE teams.team_name = 'England';`
     );
     return queryResult.rows.length > 0 ? queryResult.rows : null;
   } catch (error) {
