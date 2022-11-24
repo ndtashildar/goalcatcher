8114f95b532f522f7fad672323e24ee0d9a552ca
shankar kailas <skailas1@asu.edu>
2022-11-24 02:38:15 -0700

    initial attempt at dynamic table

--- client/src/app/routes/Landing/index.js
+++ client/src/app/routes/Landing/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
 
@@ -13,6 +13,11 @@ const Landing = () => {
 
   const [searchErrorMessage, setSearchErrorMessage] = useState("");
 
+  // state for data from db
+  const [data, setData] = useState([]);
+
+ 
+
   const onSearch = async e => {
     e.preventDefault();
     try {
@@ -26,29 +31,53 @@ const Landing = () => {
       }
       else if(!hid || hid === 0){
         const response = await fetch(`/match/${aid}`)
+        
         const matches = response.json()
-      
-        console.log(matches);
+        
+        //matches is a promise, so use then
+
+        matches.then(value => {
+          // console.log(value.payload)
+          setData(value.payload)
+          console.log(data)
+          console.log(data[0])
+        }).catch (err => {
+          console.log(err)
+        })
+        
         
       }
       else if(!aid || aid === 0){
 
         const response = await fetch(`/match/${hid}`)
         const matches = response.json()
-      
-        console.log(matches);
+        matches.then(value => {
+          // console.log(value.payload)
+          setData(value.payload)
+          console.log(data)
+          console.log(data[0])
+        }).catch (err => {
+          console.log(err)
+        })
       }
       else{
         const response = await fetch(`/match/${hid}/${aid}`)
         const matches = response.json()
-        
-        console.log(matches);
+        matches.then(value => {
+          // console.log(value.payload)
+          setData(value.payload)
+          console.log(data)
+          console.log(data[0])
+        }).catch (err => {
+          console.log(err)
+        })
       }
     } catch (error) {
       console.log(error);
     }
   }
 
+ 
   return (
     <div className="Landing">
       <div className="inner container is-fluid">
@@ -68,6 +97,32 @@ const Landing = () => {
                 Search
               </button>
             </Link>
+        </div>
+        <div className="container">
+            <h1>Simple Inventory Table</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>mid</th>
+                    <th>home_score</th>
+                    <th>away_score</th>
+                    <th>team_name</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item) => (
+                            <tr key={item.mid}>
+                                <td>{item.mid}</td>
+                                <td>{item.home_score}</td>
+                                <td>{item.away_score}</td>
+                                <td>{item.team_name}</td>
+                                <td/>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
       </div>
     </div>
